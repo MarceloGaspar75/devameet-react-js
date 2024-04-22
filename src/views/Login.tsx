a536a244f12117e48d89955d659937bcf5389a8f
Marcelo Gaspar <marcelo.gaspar@gmail.com>
2024-04-22 10:51:26 -0300

    Aula 03 Parte 02

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,7 +1,33 @@
+import { useState } from 'react';
+import logo from '../assets/images/logo.svg';
+import loginIcon from '../assets/images/mail.svg';
+import passwordIcon from '../assets/images/key.svg';
+import { PublicInput } from '../components/general/PublicInput';
 
-export const Login =() => {
+
+export const Login = () => {
+
+    const [login, setLogin] = useState('');
+    const [password, setPassword] = useState('');
 
     return (
-        <h1>Login</h1>
+        <div className="container-public">
+            <img src={logo} alt='Logo Devameet' className='logo' />
+            <form>
+                <PublicInput icon={loginIcon} alt="Email" name="Email"
+                    type="text" modelValue={login} setValue={setLogin} />
+
+                <PublicInput icon={passwordIcon} alt="Senha" name="Senha"
+                    type="password" modelValue={password} setValue={setPassword} />
+
+
+                <button type='button'>Login</button>
+
+                <div className='link'>
+                    <p> Não possui uma conta?</p>
+                    <a> Faça seu cadastro agora!</a>
+                </div>
+            </form>
+        </div>
     );
 }
