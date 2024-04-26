190f25d289e13cd8d603e177efc1dd1b9ad44f93
Marcelo Gaspar <marcelo.gaspar@gmail.com>
2024-04-26 14:17:50 -0300

    Aula 02 Parte 02

--- src/components/general/PublicInput.tsx
+++ src/components/general/PublicInput.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState } from "react";
 
 type PublicInputType = {
     icon: string,
@@ -16,22 +16,19 @@ export const PublicInput: React.FC<PublicInputType> = ({
     type,
     modelValue,
     setValue
-
 }) => {
-
     const [focus, setFocus] = useState(false);
+
     return (
-        <div className={"input" + (focus ? 'focus' : '')}>
+        <div className={"input " + (focus ? 'focus' : '')}>
             <img src={icon} alt={alt} />
             <input type={type} placeholder={name} 
                 name={name}
                 value={modelValue}
-                onChange={e => setValue(e.target.value)}
+                onChange={e => setValue(e.target.value)} 
                 onFocus={() => setFocus(true)}
                 onBlur={() => setFocus(false)}
-
                 />
-                
         </div>
     )
 }
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -3,31 +3,59 @@ import logo from '../assets/images/logo.svg';
 import loginIcon from '../assets/images/mail.svg';
 import passwordIcon from '../assets/images/key.svg';
 import { PublicInput } from '../components/general/PublicInput';
+import { LoginServices } from '../services/LoginServices';
+
+const loginServices = new LoginServices();
 
 
 export const Login = () => {
 
     const [login, setLogin] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const [loading, setLoading] =useState(false);
+
+    const doLogin = async() => {
+        try {
+            setError('');
+            if(!login || login.trim().length < 5 
+                || !password || password.length < 4){
+                 return setError('Favor preencher os campos corretamente.')
+                }   
+                
+                setLoading(true);
+                await loginServices.login({login, password});
+                setLoading(false);
+        } catch (e:any) {
+            console.log('Erro ao efetuar login:', e);
+            setLoading(false);
+            if(e?.response?.data?.message){
+            return setError(e?.response?.data?.message);
+            }
+            return setError('Erro ao efetuar login, tente novamente');
+        }
+
+    }
 
     return (
         <div className="container-public">
             <img src={logo} alt='Logo Devameet' className='logo' />
             <form>
-                <PublicInput icon={loginIcon} alt="Email" name="Email"
-                    type="text" modelValue={login} setValue={setLogin} />
+                {error && <p className='error'>{error}</p>}
+            <PublicInput icon={loginIcon} alt="Email" name="Email"
+                    type="text" modelValue={login} setValue={setLogin}/>
 
-                <PublicInput icon={passwordIcon} alt="Senha" name="Senha"
-                    type="password" modelValue={password} setValue={setPassword} />
+            <PublicInput icon={passwordIcon} alt="Senha" name="Senha"
+                    type="password" modelValue={password} setValue={setPassword}/>
 
+                 <button type='button' onClick={doLogin} disabled={loading}>{loading? '...Carregando' : 'Login'}</button>
 
-                <button type='button'>Login</button>
-
-                <div className='link'>
+                 <div className='link'>
                     <p> Não possui uma conta?</p>
                     <a> Faça seu cadastro agora!</a>
                 </div>
             </form>
         </div>
+      
     );
 }
