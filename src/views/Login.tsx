443f358c3f40ef79f292f5813d1577c3c2745ed8
Marcelo Gaspar <marcelo.gaspar@gmail.com>
2024-05-01 08:43:41 -0300

    Aula 02 Parte 03

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -4,6 +4,7 @@ import loginIcon from '../assets/images/mail.svg';
 import passwordIcon from '../assets/images/key.svg';
 import { PublicInput } from '../components/general/PublicInput';
 import { LoginServices } from '../services/LoginServices';
+import { Link, useSearchParams } from 'react-router-dom';
 
 const loginServices = new LoginServices();
 
@@ -14,6 +15,9 @@ export const Login = () => {
     const [password, setPassword] = useState('');
     const [error, setError] = useState('');
     const [loading, setLoading] =useState(false);
+
+    const [searchParams] = useSearchParams();
+    const success = searchParams.get('success');
 
     const doLogin = async() => {
         try {
@@ -42,6 +46,7 @@ export const Login = () => {
             <img src={logo} alt='Logo Devameet' className='logo' />
             <form>
                 {error && <p className='error'>{error}</p>}
+                {success && <p className='success'>Cadastro efetuado com sucesso, faça seu login</p>}
             <PublicInput icon={loginIcon} alt="Email" name="Email"
                     type="text" modelValue={login} setValue={setLogin}/>
 
@@ -52,7 +57,7 @@ export const Login = () => {
 
                  <div className='link'>
                     <p> Não possui uma conta?</p>
-                    <a> Faça seu cadastro agora!</a>
+                    <Link to='/register'> Faça seu cadastro agora!</Link>
                 </div>
             </form>
         </div>
