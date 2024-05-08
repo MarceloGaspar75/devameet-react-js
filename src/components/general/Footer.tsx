01bb0ddd434aa689ee11fd63d09e1536b5c2b076
Marcelo Gaspar <marcelo.gaspar@gmail.com>
2024-05-08 07:22:13 -0300

    Aula 03 Parte 03

---
+++ src/components/general/Footer.tsx
@@ -0,0 +1,12 @@
+import { Navigation } from "./Navigation";
+
+export const Footer = () =>{
+
+    return (
+        <div className="container-footer">
+            <Navigation />
+        </div>
+
+        
+    );
+}
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,13 @@
+import { Footer } from "../components/general/Footer";
 import { Header } from "../components/general/Header";
+import { MeetHome } from "../components/general/meet/MeetHome";
 
 export const Home = () => {
     return (
+        <>
         <Header />
+        <MeetHome />
+        <Footer />
+        </>
     );
 }
